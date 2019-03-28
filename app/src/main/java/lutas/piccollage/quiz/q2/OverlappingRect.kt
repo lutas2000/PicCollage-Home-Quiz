200ab6e0d543c12eebc21b0e1a08b81ab0b05295
Lutas <wsrew2000@gmail.com>
2019-03-28 18:41:15 +0800

    complete Q2

--- app/src/main/java/lutas/piccollage/quiz/q2/OverlappingRect.kt
+++ app/src/main/java/lutas/piccollage/quiz/q2/OverlappingRect.kt
@@ -1,11 +1,26 @@
 package lutas.piccollage.quiz.q2
 
-import android.graphics.Rect
-
 class OverlappingRect {
 
-    fun checkOverlapping(rectA: Rect, rectB: Rect): Boolean {
-        // TODO
-        return false
+    fun checkOverlapping(rectA: RotatableRect, rectB: RotatableRect): Boolean {
+        val axes = rectA.getAxes() + rectB.getAxes()
+
+        for (axis in axes) {
+            if (!overlap(rectA, rectB, axis)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun overlap(rectA: RotatableRect, rectB: RotatableRect, axis: RotatableRect.Point): Boolean {
+        val projectionA = rectA.getPoints().map { axis.dot(it) }.sorted()
+        val minA = projectionA[0]
+        val maxA = projectionA[3]
+        val projectionB = rectB.getPoints().map { axis.dot(it) }.sorted()
+        val minB = projectionB[0]
+        val maxB = projectionB[3]
+
+        return !(maxB < minA || maxA < minB)
     }
 }
--- app/src/test/java/lutas/piccollage/quiz/q2/OverlappingRectTest.kt
+++ app/src/test/java/lutas/piccollage/quiz/q2/OverlappingRectTest.kt
@@ -1,6 +1,5 @@
 package lutas.piccollage.quiz.q2
 
-import android.graphics.Rect
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -10,24 +9,40 @@ class OverlappingRectTest {
 
     @Test
     fun test_overlapping() {
-        val rectA = Rect(0, 0, 100, 100)
-        val rectB = Rect(50, 0, 150, 100)
+        val rectA = RotatableRect(0.0, 0.0, 100.0, 100.0)
+        val rectB = RotatableRect(50.0, 0.0, 150.0, 100.0)
         val isOverlapping = checker.checkOverlapping(rectA, rectB)
         assertEquals(true, isOverlapping)
     }
 
     @Test
     fun test_intersecting() {
-        val rectA = Rect(0, 0, 100, 100)
-        val rectB = Rect(100, 0, 200, 100)
+        val rectA = RotatableRect(0.0, 0.0, 100.0, 100.0)
+        val rectB = RotatableRect(100.0, 0.0, 200.0, 100.0)
         val isOverlapping = checker.checkOverlapping(rectA, rectB)
         assertEquals(true, isOverlapping)
     }
 
     @Test
     fun test_independent() {
-        val rectA = Rect(0, 0, 100, 100)
-        val rectB = Rect(101, 0, 201, 100)
+        val rectA = RotatableRect(0.0, 0.0, 100.0, 100.0)
+        val rectB = RotatableRect(101.0, 0.0, 201.0, 100.0)
+        val isOverlapping = checker.checkOverlapping(rectA, rectB)
+        assertEquals(false, isOverlapping)
+    }
+
+    @Test
+    fun test_rotate_overlapping() {
+        val rectA = RotatableRect(0.0, 0.0, 100.0, 500.0, 45.0)
+        val rectB = RotatableRect(110.0, 0.0, 210.0, 500.0, -45.0)
+        val isOverlapping = checker.checkOverlapping(rectA, rectB)
+        assertEquals(true, isOverlapping)
+    }
+
+    @Test
+    fun test_rotate_independent() {
+        val rectA = RotatableRect(0.0, 0.0, 100.0, 500.0, -10.0)
+        val rectB = RotatableRect(190.0, 0.0, 290.0, 500.0, 10.0)
         val isOverlapping = checker.checkOverlapping(rectA, rectB)
         assertEquals(false, isOverlapping)
     }
