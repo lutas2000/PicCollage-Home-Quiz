c3d45544dc046abc7b0277f62b9372b896be17c6
Lutas <wsrew2000@gmail.com>
2019-03-28 09:35:00 +0800

    Complete Q1

--- app/src/main/java/lutas/piccollage/quiz/q1/Containing7.kt
+++ app/src/main/java/lutas/piccollage/quiz/q1/Containing7.kt
@@ -3,7 +3,41 @@ package lutas.piccollage.quiz.q1
 class Containing7 {
 
     fun compute(num: Int): Int {
-        // TODO
-        return 0
+        if (num < 7)
+            return 0
+
+        val digits = Math.log10(num.toDouble()).toInt()
+        // g(1) = 0
+        // g(10) = 1
+        val a = IntArray(digits + 2)
+        a[0] = 0
+        a[1] = 1
+
+        for (i in 2..digits) {
+            a[i] = a[i - 1] * 9 + Math.pow(10.0, (i - 1).toDouble()).toInt()
+        }
+
+        val p = Math.pow(10.0, digits.toDouble()).toInt()
+        val msd = num / p
+
+        return when {
+            msd == 7 -> msd * a[digits] + num % p + 1
+            msd > 7 -> (msd - 1) * a[digits] + p + compute(num % p)
+            else -> (msd) * a[digits] + compute(num % p)
+        }
+    }
+
+    /**
+     * find g(10000) to assert
+     */
+    fun stupidWay(num: Int) {
+        var sevens = 0
+        for (i in 1..num) {
+            val string = i.toString()
+            if (string.contains('7')) {
+                sevens++
+            }
+        }
+        println("result: $sevens")
     }
 }
--- app/src/test/java/lutas/piccollage/quiz/q1/Containing7Test.kt
+++ app/src/test/java/lutas/piccollage/quiz/q1/Containing7Test.kt
@@ -8,8 +8,8 @@ class Containing7Test {
     @Test
     fun is_computer_correct() {
         val computer = Containing7()
-        assertEquals(0, computer.compute(5))
-        assertEquals(1, computer.compute(7))
-        assertEquals(19, computer.compute(100))
+        assertEquals(2, computer.compute(17))
+        assertEquals(242, computer.compute(871))
+        assertEquals(3439, computer.compute(10000))
     }
 }
